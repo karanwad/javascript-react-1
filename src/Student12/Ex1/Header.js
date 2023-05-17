1d235204aa6f96e016166e0c3d7c33bd3a8508ef
Karan Wadhwani <kwadhwani@xpanxion.com>
2023-05-17 12:46:42 -0400

    finish ex1

---
+++ src/Student12/Ex1/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <>
+    <div className="Dollar">Dollar Food Store</div>
+    <div className="Links">Home | Login | Support | FAQ</div>
+    </>
+  )
+}
+
+export default Header
