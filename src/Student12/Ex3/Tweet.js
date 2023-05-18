bafe0e9857a3635a50cad44f4c49fc3e5460c9af
Karan Wadhwani <kwadhwani@xpanxion.com>
2023-05-18 14:45:48 -0400

    Ex. 4: Like Counter

---
+++ src/Student12/Ex3/Tweet.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const Tweet = ({ message, color }) => {
+  return <div style={{ backgroundColor: `${color}` }}>{message}</div>;
+};
--- src/Student12/Ex4/Main.js
+++ src/Student12/Ex4/Main.js
@@ -1,10 +1,31 @@
-import React from 'react'
-import './Main.css'
-
+import React from "react";
+import "./Main.css";
+import { Tweet } from "./Tweet";
+import { useState } from "react";
 export const Main = () => {
+  const [like, setLike] = useState(0);
+  const clickHandler = () => {
+    setLike(like + 1);
+  };
   return (
-    <div>
-        Hello I'm student id: 1.
+    <div className="Tweets">
+      {" "}
+      <div>Total Likes: {like}</div>{" "}
+      <Tweet
+        message="It's payday!!!"
+        color="green"
+        clickHandler={clickHandler}
+      />{" "}
+      <Tweet
+        message="I just bought a new car!"
+        color="yellow"
+        clickHandler={clickHandler}
+      />{" "}
+      <Tweet
+        message="I just smashed my new car."
+        color="red"
+        clickHandler={clickHandler}
+      />{" "}
     </div>
-  )
-}
+  );
+};
